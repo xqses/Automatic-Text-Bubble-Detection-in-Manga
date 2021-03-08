bf22400b609d10645b83c2481d508e85f339bb12
n3reides <dahlstedtolle@gmail.com>
2021-03-08 15:31:41 +0100

    added some more samples, and a sample image

--- project1.py
+++ project1.py
@@ -6,6 +6,7 @@ from pythonRLSA import rlsa
 from scipy.ndimage import label
 import os
 from sklearn import ensemble
+from matplotlib import pyplot as plt
 
 
 def train_classifier():
@@ -79,25 +80,26 @@ tree = train_classifier()
 ## file_name = os.path.join(file_path, file)
 ## counter += 1
 ## print("cleaning image " + str(counter) + " to path "+ file_path + '_output_' + file)
-## open_file = open("samples.txt", "r+")
+# open_file = open("samples.txt", "r+")
 ## beginning
-file_name = '104.jpg'
+file_name = '9_main-qimg-de9e1056b4cd97bbf39f1c8b4ba68f6a.jpg'
 img = cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
 img_rlsa_horizontal = rlsa.rlsa(thresh, True, False, 10)
 img_rlsa_vertical = rlsa.rlsa(img_rlsa_horizontal, False, True, 15)
+# cv2.imwrite("1_rlsa-smoothed.jpg", img_rlsa_vertical)
 
 opening = cv2.morphologyEx(img_rlsa_vertical, cv2.MORPH_OPEN, np.ones((3, 3), np.int), iterations=2)
 
-cv2.imwrite('placeholder2.jpg', opening)
+# cv2.imwrite('2_opened_image.jpg', opening)
 
 sure_bg = cv2.dilate(opening, None, iterations=5)
 sure_bg = sure_bg - cv2.erode(sure_bg, None)
 dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
 dist_transform = ((dist_transform - dist_transform.min()) / (dist_transform.max() - dist_transform.min()) * 255).astype(
     np.uint8)
-cv2.imwrite('place6.jpg', sure_bg)
-cv2.imwrite('placeholder3.jpg', dist_transform)
+# cv2.imwrite('3_dilated_eroded_background.jpg', sure_bg)
+# cv2.imwrite('4_distance_transformed.jpg', dist_transform)
 ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
 
 lbl, ncc = label(dist_transform)
@@ -117,9 +119,9 @@ result = 255 - lbl
 result[result != 255] = 0
 result = cv2.dilate(result, None)
 ret, thresh_res = cv2.threshold(result, 0, 255, cv2.THRESH_BINARY_INV)
-cv2.imwrite('placeholder4.jpg', thresh_res)
+# cv2.imwrite('5_thresholded_watershed_regions.jpg', thresh_res)
 img_3C[result == 255] = (0, 0, 255)
-cv2.imwrite('placeholder5.jpg', img_3C)
+# cv2.imwrite('6_segmented_regions_colored.jpg', img_3C)
 
 # find contours
 contours, hierarch = cv2.findContours(result, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -131,22 +133,27 @@ for cont in contours:
     imgW = new_cont.shape[1]
     x, y, w, h = cv2.boundingRect(cont)
     if not (not (w > 30) or not (h > 50)) and h < 0.75 * imgH and w < 0.75 * imgW:
+        # for debugging purposes (finding out why some bubbles are not detected)
+        # cv2.drawContours(new_cont,[cont], -1, (0,255,0), 3)
+        # plt.imshow(new_cont, interpolation='bicubic')
+        # plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+        # plt.show()
         is_text = detect_text(thresh, cont)
         if is_text:
             cont_counter += 1
             cv2.drawContours(new_cont, [cont], -1, (0, 255, 0), 3)
-            cv2.imwrite('written_cont' + str(cont_counter) + '.jpg', new_cont)
+            # cv2.imwrite('written_contour' + str(cont_counter) + '.jpg', new_cont)
             list_of_bubbles.append(cont)
             print(cont_counter)
             ## print(x,y,w,h)
 
 # img_3C = cv2.imread(file_name)
 
-# for bubble in list_of_bubbles:
-#    cv2.drawContours(img_3C, [bubble], -1, (255,255,255), thickness = -1)
-# status = cv2.imwrite(file_path + '_output_' + file, img_3C)
-# print("written status " + str(status))
-## open_file.close()
+for bubble in list_of_bubbles:
+   cv2.drawContours(img, [bubble], -1, (255,255,255), thickness = -1)
+
+status = cv2.imwrite('9_output_.jpg', img)
+print("written status " + str(status))
 
 # print("total bubbles predicted by human values: " + str(count_human_true))
 # print("total bubbles predicted by decision tree: " + str(count_tree_true))
